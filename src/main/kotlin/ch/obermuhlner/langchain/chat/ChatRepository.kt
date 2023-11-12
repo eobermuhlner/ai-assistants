0300b21b39ce50921c339fd28f83db210f861dee
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2023-11-12 20:12:56 +0100

    add findByUser()

--- src/main/kotlin/ch/obermuhlner/langchain/chat/ChatRepository.kt
+++ src/main/kotlin/ch/obermuhlner/langchain/chat/ChatRepository.kt
@@ -1,7 +1,11 @@
 package ch.obermuhlner.langchain.chat
 
+import ch.obermuhlner.langchain.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ChatRepository : JpaRepository<Chat, Long>
+interface ChatRepository : JpaRepository<Chat, Long> {
+
+    fun findByUser(user: User): List<Chat>
+}
