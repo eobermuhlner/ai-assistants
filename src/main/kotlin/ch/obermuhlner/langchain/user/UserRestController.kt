9d13c4c2458e3a5237e066b3a9b73425c4e72654
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2023-11-12 20:18:52 +0100

    cosmetics

--- src/main/kotlin/ch/obermuhlner/langchain/user/UserRestController.kt
+++ src/main/kotlin/ch/obermuhlner/langchain/user/UserRestController.kt
@@ -7,23 +7,22 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/users")
-class UserRestController @Autowired constructor(private val userService: UserService) {
+class UserRestController @Autowired constructor(
+    private val userService: UserService
+) {
 
-    // Create a new user
     @PostMapping
     fun createUser(@RequestBody user: User): ResponseEntity<User> {
         val createdUser = userService.createUser(user)
         return ResponseEntity(createdUser, HttpStatus.CREATED)
     }
 
-    // Retrieve all users
     @GetMapping
     fun findAllUsers(): ResponseEntity<List<User>> {
         val users = userService.findAllUsers()
         return ResponseEntity(users, HttpStatus.OK)
     }
 
-    // Retrieve a user by ID
     @GetMapping("/{id}")
     fun findUserById(@PathVariable id: Long): ResponseEntity<User?> {
         val user = userService.findUserById(id)
@@ -34,7 +33,6 @@ class UserRestController @Autowired constructor(private val userService: UserSer
         }
     }
 
-    // Update a user by ID
     @PutMapping("/{id}")
     fun updateUserById(@PathVariable id: Long, @RequestBody updatedUser: User): ResponseEntity<User?> {
         val user = userService.updateUserById(id, updatedUser)
@@ -45,7 +43,6 @@ class UserRestController @Autowired constructor(private val userService: UserSer
         }
     }
 
-    // Delete a user by ID
     @DeleteMapping("/{id}")
     fun deleteUserById(@PathVariable id: Long): ResponseEntity<Unit> {
         val deleted = userService.deleteUserById(id)
