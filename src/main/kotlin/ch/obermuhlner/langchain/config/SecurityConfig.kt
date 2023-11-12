d23e720ede12d5085d8eb20f32e01ec034b55768
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2023-11-12 20:18:27 +0100

    add SecurityConfig

---
+++ src/main/kotlin/ch/obermuhlner/langchain/config/SecurityConfig.kt
@@ -0,0 +1,28 @@
+package ch.obermuhlner.langchain.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .authorizeHttpRequests { it.requestMatchers("/api/users/**").hasRole("ADMIN") }
+            .authorizeHttpRequests { it.anyRequest().authenticated() }
+            .csrf { it.ignoringRequestMatchers("/api/**") }
+            .httpBasic(Customizer.withDefaults())
+            .build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return NoOpPasswordEncoder.getInstance()
+    }
+}
