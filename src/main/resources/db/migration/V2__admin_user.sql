e1a3a5077a199ac96bf6d64dcee6671da42b8e12
Eric Obermühlner <eric.obermuehlner@assetmax.ch>
2023-11-12 20:19:06 +0100

    create default admin user

---
+++ src/main/resources/db/migration/V2__admin_user.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, password, name, openai_api_key, description, authorities)
+VALUES ('admin', 'admin', 'Administrator', 'demo', 'The administrator that can manage users.', 'ROLE_ADMIN');
